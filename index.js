40c98a6c4a71d7a870dc8701ef16b154db173480
Thomas Doyle <tmdoyle11@gmail.com>
2022-03-01 15:22:21 -0600

    commit tree 16e736a3a41fd973bd8865b0ea258f808f27c431

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph) {
+    cats.push(Ralph);
+}
+
+function destructivelyPrependCat(Bob) {
+    cats.unshift(Bob);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(Broom) {
+    let newArray = [...cats];
+    newArray.push(Broom);
+    return newArray;
+}
+
+function prependCat(Arnold) {
+    let newArray1 = [...cats];
+    newArray1.unshift(Arnold);
+    return newArray1; 
+}
+
+function removeLastCat() {
+    let newArray2 = [...cats];
+    newArray2.pop();
+    return newArray2;
+}
+
+function removeFirstCat() {
+    let newArray3 = [...cats];
+    newArray3.shift();
+    return newArray3;
+}
